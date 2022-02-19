6da1ce869a80d46064e0a9777cbf3d0bafdc79a4
hamitmizrak <hamitmizrak@gmail.com>
2022-02-19 18:23:57 +0300

    maven Jar oluşturma

---
+++ src/main/java/com/hamitmizrak/maven/MavenTutorials.java
@@ -0,0 +1,12 @@
+package com.hamitmizrak.maven;
+
+public class MavenTutorials {
+
+    //  JAR OLUSTURMA
+    // ./mvnw clean package -DskipTests
+
+    //  mvn clean install
+
+    // TEST
+    // mvn  clean install   -DskipTest=true
+}
