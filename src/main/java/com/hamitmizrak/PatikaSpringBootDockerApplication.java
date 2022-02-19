be87a436a22783775b800f070b2b6f20489b3299
hamitmizrak <hamitmizrak@gmail.com>
2022-02-19 15:24:49 +0300

    AuditorAwareBean, AuditorAwareImpl, BaseEntityAudit, @SpringBootApplication

--- src/main/java/com/hamitmizrak/PatikaSpringBootDockerApplication.java
+++ src/main/java/com/hamitmizrak/PatikaSpringBootDockerApplication.java
@@ -1,10 +1,18 @@
 package com.hamitmizrak;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
-public class PatikaSpringBootDockerApplication {
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@Log4j2
+public class PatikaSpringBootDockerApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(PatikaSpringBootDockerApplication.class, args);
