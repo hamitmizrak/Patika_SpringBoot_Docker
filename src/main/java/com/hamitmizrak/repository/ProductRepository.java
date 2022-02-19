9eab82163cd7ed8ff62e2c6dcad0235bd3ad256e
hamitmizrak <hamitmizrak@gmail.com>
2022-02-19 16:20:06 +0300

    @Repository

--- src/main/java/com/hamitmizrak/repository/ProductRepository.java
+++ src/main/java/com/hamitmizrak/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.hamitmizrak.repository;
 
+import com.hamitmizrak.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository {
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
