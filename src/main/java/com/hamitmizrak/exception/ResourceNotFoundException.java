8ac98c3de049560ba41d5587db13ef23c88da61f
hamitmizrak <hamitmizrak@gmail.com>
2022-02-19 16:24:29 +0300

    ResourceNotFoundException

---
+++ src/main/java/com/hamitmizrak/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    //constructor:
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
